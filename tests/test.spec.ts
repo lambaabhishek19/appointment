4408d95c71973f4894e43a17015c65f33e8803ff
lambaabhishek19 <60145202+lambaabhishek19@users.noreply.github.com>
2026-06-23 00:50:27 +0200

    Create test.spec.ts

---
+++ tests/test.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://ssc.wilkencloud.de/ulm/select2?md=4');
+  await page.locator('#button-plus-600').click();
+   await page.locator('#WeiterButton').click();
+   await page.locator('#OKButton').click();
+ await page.locator('#WeiterButton').click();
+  await expect( page.getByText('Kein freier Termin verfügbar')).toBeVisible();
+});
